b39f00882d3bfc2ee14ad6fdaf4828fb72fd7b68
Leonard <lordknight1904@gmail.com>
2022-12-02 21:11:36 +0900

    first

--- autobot/base.py
+++ autobot/base.py
@@ -1,1 +1,11 @@
 import torch
+from torch import nn
+
+
+class MultiHeadAttention(nn.Module):
+
+    class __init__(self):
+        super().__init__()
+        
+    def forward(self, x):
+        return x
