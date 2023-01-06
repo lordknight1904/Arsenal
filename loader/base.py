21834818fa7a8f72e3fbaf2c3098220969fbd779
Leonard <lordknight1904@gmail.com>
2023-01-06 18:22:08 +0900

    left hand coordinate system, clockwise rotation, row major

--- loader/base.py
+++ loader/base.py
@@ -1,5 +1,5 @@
 # %%
-from dataclasses import dataclass
+from dataclasses import dataclass, fields
 import numpy as np
 import PIL
 from PIL import Image
@@ -8,6 +8,16 @@ from PIL import Image
 @dataclass(slots=True)
 class InputImage:
     image: np.ndarray
+
+    def _validate_attr(self):
+        if not isinstance(self.image, np.ndarray):
+            print(f'Expect image of type ndarray but got {type(self.image)}')
+
+        if (dim := len(self.image.shape)) != 3:
+            print(f'Expect image with dimension of 3 but got {dim}')
+
+    def __post_init__(self):
+        self._validate_attr()
 
     @classmethod
     def from_path(cls, path: str):
